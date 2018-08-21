7c46481a0b1e10a136e1497368df3205fd71986e
Robert Sumner Jr <rsumner868@gmail.com>
2018-08-20 22:26:51 -0500

    Init commit
    Conflicts:
    	.babelrc
    	.gitignore
    	__tests__/measures.test.js
    	extension/devtools.html
    	extension/load.html
    	extension/load.js
    	extension/manifest.json
    	package.json
    	src/index.js
    	yarn.lock

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import { ReactPerfDevtool } from './extension/components/ReactPerfDevtool'
 import { registerObserver } from './npm/hook'
 
 export { ReactPerfDevtool, registerObserver }
+module.exports = require('./components/ReactPerfPanel')
